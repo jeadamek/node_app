00733dd761f8d6f6c11fea257d5a3adb01ab8f44
jessica <jessica.g.adamek@gmail.com>
2023-02-06 15:57:22 -0300

    add database connection and users routes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,33 @@
+require("express-async-errors")
+// const migrationsRun = require("./database/")
+const AppError = require("./utils/AppError");
+
+
 const express = require('express');
+const routes = require("./routes");
+
 const app = express();
+app.use(express.json());
 
 
+app.use(routes);
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-})
+app.use((error, request, response, next) => {
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+});
 
 
 const PORT = 3333;
-app.listen(PORT, () => console.log(`Sever is running in port ${PORT}`))
-
+app.listen(PORT, () => console.log(`Sever is running in port ${PORT}`));
